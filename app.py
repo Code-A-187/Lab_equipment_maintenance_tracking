9c258884900b5a3a865d53d49fce2f0373cffe99
Anton Simeonov <aplamenov@gmail.com>
2026-07-17 20:31:17 +0300

    feat: check if id already exists and return message. Automate id. Calculate cost by status criteria

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 lab_equipment_list = [
     {"id": 1, "name": "Centrifuge", "status": "active", "maintenance_cost":500},
     {"id": 2, "name": "Universal Oven", "status": "active", "maintenance_cost": 450},
-    {"id": 3, "name": "Balance", "status": "broken", "maintenance_cost": 530}
+    {"id": 3, "name": "Balance", "status": "broken", "maintenance_cost": 530},
+    {'id': 4, 'name': 'Furnace', 'status': 'active', 'maintenance_cost': 250}
 ]
 
-def add_equipment(lst, id, name, status, maintenance_cost):
+def add_equipment(lst, name, status, maintenance_cost):
+    id = max([i["id"] for i in lst]) + 1 # if some id is deleted will always get the max id
     equipment = {"id": id, "name": name, "status": status, "maintenance_cost": maintenance_cost}
-    return lst.append(equipment)
-
+    for i in lst:
+        if i["id"] == equipment["id"]:
+            return "Equipment with same ID already in the list"
+    
+    lst.append(equipment)
+    return lst
+    
 def list_broken_equipment(lst):
     list_broken = []
     for i in lst:
@@ -16,13 +23,26 @@ def list_broken_equipment(lst):
     
     return list_broken
 
-def maintenance_cost_sum(lst):
+def list_active_equipment(lst):
+    return [i for i in lst if i["status"] == "active"]
+
+def maintenance_cost_sum(lst, status):
     total = 0
-    for i in lst:
-        total += i["maintenance_cost"]
-    
+    if status == "active":
+        active_list = list_active_equipment(lst)
+        for i in active_list:
+            total += i["maintenance_cost"]
+    elif status == "broken":
+        broken_list =  list_broken_equipment(lst)
+        for i in broken_list:
+            total += i["maintenance_cost"]
+    else:
+        for i in lst:
+            total += i["maintenance_cost"]
+
     return total
 
-print(add_equipment(lab_equipment_list, 4, "Furnace", "active", 250))
+print(add_equipment(lab_equipment_list, "HPLC", "broken", 450))
 print(list_broken_equipment(lab_equipment_list))
-print(maintenance_cost_sum(lab_equipment_list))
+print(list_active_equipment(lab_equipment_list))
+print(maintenance_cost_sum(lab_equipment_list, ""))
